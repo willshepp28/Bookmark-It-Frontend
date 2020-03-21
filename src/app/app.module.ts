7f99c291fc05eae6d7af1976fbe9be94ed256028
Will Sheppard <28759252+willshepp28@users.noreply.github.com>
2020-03-21 13:32:31 -0400

    created home, bookmark components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './core/components/home/home/home.component';
+import { CreateComponent } from './core/components/create/create/create.component';
+import { BookmarkComponent } from './core/components/bookmark/bookmark/bookmark.component';
+import { HeaderComponent } from './core/components/header/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    CreateComponent,
+    BookmarkComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
