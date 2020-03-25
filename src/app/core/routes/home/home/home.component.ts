afbfa6eee15b1c6d6e852267fdc3ab549d2b4f31
Will Sheppard <28759252+willshepp28@users.noreply.github.com>
2020-03-24 22:25:58 -0400

    refactored code for reusablity, and readability in home.component.ts; constructed error or success message in addNewTopic and addNewBookmark functions in home.component.ts; form now clears when user enters data

--- src/app/core/routes/home/home/home.component.ts
+++ src/app/core/routes/home/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { BookmarksService } from 'src/app/core/services/bookmarks/bookmarks.service';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import { TopicsService } from 'src/app/core/services/topics/topics.service';
+import { finalize } from 'rxjs/operators';
 
 
 @Component({
@@ -13,6 +14,16 @@ export class HomeComponent implements OnInit {
   topics: any = [];
   createTopic: FormGroup;
   createBookmark: FormGroup;
+  showTopicMessage = {
+    show: false,
+    message: false
+
+  };
+  showBookmarkMessage = {
+    show: false,
+    message: false
+  };
+
 
   constructor(
     public bookmarkService: BookmarksService,
@@ -44,12 +55,52 @@ export class HomeComponent implements OnInit {
 
 
   addNewTopic() {
-    return this.topicService.createTopic(this.createTopic.value).subscribe();
+    return this.topicService.createTopic(this.createTopic.value).pipe(
+      finalize(() => this.clearFormAndRemoveMessage('createTopic', 'showTopicMessage'))
+    ).subscribe(
+    success => {
+      this.setMessageVisiblityAndStatus('showTopicMessage', true, true);
+    },
+    error => {
+      this.setMessageVisiblityAndStatus('showTopicMessage', true, false);
+    });
   }
 
 
   addNewBookmark() {
-    return this.bookmarkService.createBookmark(this.createBookmark.value).subscribe();
+    return this.bookmarkService.createBookmark(this.createBookmark.value).pipe(
+      finalize(() =>  this.clearFormAndRemoveMessage('createBookmark', 'showBookmarkMessage'))
+    ).subscribe(
+    success => {
+      this.setMessageVisiblityAndStatus('showBookmarkMessage', true, true);
+    },
+    error => {
+      this.setMessageVisiblityAndStatus('showBookmarkMessage', true, false);
+    }
+    );
+  }
+
+  clearFormAndRemoveMessage(formName, messageName) {
+    this.clearForm(formName);
+    this.removeMessage(messageName, 2000);
+  }
+
+
+  setMessageVisiblityAndStatus(messageName, showStatus, messageStatus){
+    this[messageName].show = showStatus;
+    this[messageName].message = messageStatus;
+  }
+
+
+  clearForm(formName) {
+    this[formName].reset();
+  }
+
+
+  removeMessage(messageName, time){
+    setTimeout(() => {
+      this[messageName].show = false;
+    }, time);
   }
 
 }
